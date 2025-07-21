a1ca9d557e900997b8df9adb720e0e49730c102f
Aziz <azizhoumani@gmail.com>
2025-07-21 14:34:19 +0200

    Utiliser les CSS Modules

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import styles from "./pricing.module.css"
+
+function page() {
+    console.log(styles);
+  return (
+    <div>
+      <h1 className={styles.title}>Pricing</h1>
+    </div>
+  )
+}
+
+export default page
