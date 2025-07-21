bbebac834f79b50415392f1f4b0114b4dcef29de
Aziz <azizhoumani@gmail.com>
2025-07-21 15:10:22 +0200

    Utiliser Sass avec Next _fonctionnement avec styliser un composant card

---
+++ src/app/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import "./Card.scss"
+
+function Card() {
+  return (
+    <div className="card"><p>Card</p></div>
+  )
+}
+
+export default Card
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+// import Image from "next/image";
+
+import Card from "./components/Card/Card";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl font-mono">Styles et optimisation</h1>
+      <Card />
       </main>
   );
 }
